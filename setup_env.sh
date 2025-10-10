a468e0f51cb08a13487fbfa0c75ed90128d63738
Sebastian Heusinger <sheusinger@student.ethz.ch>
2025-10-10 11:31:25 +0200

    script to install all the necesarry dependencies

---
+++ setup_env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source ~/miniconda3/etc/profile.d/conda.sh
+conda create -y -n sast python=3.9 pip
+conda activate sast
+conda config --set channel_priority flexible
+
+CUDA_VERSION=11.8
+
+echo "Installing conda stuff..."
+conda install -y h5py=3.8.0 blosc-hdf5-plugin=1.0.0 scipy \
+hydra-core=1.3.2 einops=0.6.0 torchdata=0.6.0 tqdm numba timm \
+-c conda-forge
+
+pip install torch==2.0.0 torchvision==0.15.1 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu118
+
+conda install -yc conda-forge fairscale
+
+
+python -m pip install pytorch-lightning==1.8.6 wandb==0.14.0 \
+pandas==1.5.3 plotly==5.13.1 opencv-python==4.6.0.66 tabulate==0.9.0 \
+pycocotools==2.0.6 bbox-visualizer==0.1.0 StrEnum==0.4.10
+
+conda install -y nvidia/label/cuda-11.8.0::cuda-nvcc
+git clone https://github.com/facebookresearch/detectron2.git 
+python -m pip install -e detectron2
+
+python models/detection/lwdetr/ops/setup.py build install
+conda install -y "numpy<2.0"
